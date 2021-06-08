dcb3df5d58d832a65c22ceb5a4854bb957ea8e5c
hmartiins <hmartins224@gmail.com>
2021-06-08 18:03:30 -0300

    create props for post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 
 import Feather from '@expo/vector-icons/FontAwesome5';
 
@@ -14,29 +14,45 @@ import {
   IconsContainer
 } from './styles';
 
-export function Post(){
+interface HeaderProps {
+  name: string;
+  profile_image: string;
+  hours: string;
+  legend?: string;
+  image?: string;
+  comment: string;
+  likes: string
+}
+
+export function Post({
+  name,
+  profile_image,
+  hours,
+  legend,
+  image,
+  comment,
+  likes
+}: HeaderProps){
   return (
     <>
       <Container>
         <View style={{width: '20%'}}>
-          <ProfilePhoto source={{ uri: 'https://avatars.githubusercontent.com/u/51277667?v=4' }} />
+          <ProfilePhoto source={{ uri: `${profile_image}` }} />
         </View>
         <ContentContainer>
-          <Name>Pedro Henrique  </Name>
-          <TimeToPost>•  10h</TimeToPost>
-          <Legend>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente exercitationem, obcaecati quae facilis enim iusto nam laboriosam itaque dolorum eveniet quibusdam. Possimus sed alias nisi hic accusantium ullam velit ab!
-          </Legend>
-          <PostPhoto source={{ uri: 'https://cdn4.ecycle.com.br/cache/images/2021-04/50-650-o-que-e-paisagem.jpg' }} />
+          <Name>{name}  </Name>
+          <TimeToPost>•  {hours}h</TimeToPost>
+          {legend && <Legend>{legend}</Legend>}
+          {image && <PostPhoto source={{ uri: `${image}` }} />}
           <IconsContainer>
             <TouchableOpacity>
               <Feather iconStyle={{padding: 10}} name="comment" color={"#404B5A"} size={15}>
-              ⠀27
+              ⠀{comment}
               </Feather>
             </TouchableOpacity>
             <TouchableOpacity>
               <Feather name="heart" color={"#404B5A"} size={15}>
-              ⠀312
+              ⠀{likes}
               </Feather>
             </TouchableOpacity>
           </IconsContainer>
