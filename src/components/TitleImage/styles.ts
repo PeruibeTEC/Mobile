0f03a051295024019129b3cf6d9b149e5a0a90fd
devLeSilverio <leticia.silverio09@gmail.com>
2021-06-10 02:31:42 -0300

    feat(global): created titleimage component

---
+++ src/components/TitleImage/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #ffff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'Inter_600SemiBold';
+  font-size: 24px;
+  color: #000000;
+  margin-top: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  width: 375px;
+  height: 203px;
+  margin-top: 20px;
+`;
--- src/screens/TheftItems/index.tsx
+++ src/screens/TheftItems/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
 
 import { Header } from '../../components/Header/Header';
-
+import { TitleImage } from '../../components/TitleImage';
 
 export function TheftItems() {
   return (
     <ScrollView>
-        <Text>Stolen</Text>
+      <Header title="Detalhes do Incidente" />
+      <TitleImage
+        title="Assalto a Mão Armada"
+        image="https://static.lared.cl/wp-content/uploads/2019/09/05102425/20160927185301_740x3705509366827156214087-700x366.jpg"
+       />
     </ScrollView>
   );
 }
