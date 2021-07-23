564244ea014c730e937053a1a3725afe88a934b1
hmartiins <hmartins224@gmail.com>
2021-07-23 20:52:48 -0300

    fix: resolving errors for eslint

--- src/screens/Restaurants.tsx
+++ src/screens/Restaurants.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 import fonts from '../styles/fonts';
 
