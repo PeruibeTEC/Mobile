55a5c57b6587ef1b5174f9dbb3b159e96466e848
João Victor da Silva <jv782063@gmail.com>
2021-10-07 17:21:24 -0300

    feat(header): open the drawer with the header button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native';
 
 import pixelToDP from '../../utils/pixelToDP';
@@ -8,12 +9,23 @@ import { Container, MenuIcon } from './styles';
 import Logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => {
+          navigation.dispatch(DrawerActions.openDrawer);
+        }}
+      >
+        <MenuIcon name="menu" />
+      </TouchableOpacity>
+
       <Logo height={pixelToDP.height(40)} width={pixelToDP.width(35)} />
 
       <TouchableOpacity activeOpacity={0.8} onPress={() => {}}>
-        <MenuIcon name="menu" />
+        <MenuIcon name="bell-outline" />
       </TouchableOpacity>
     </Container>
   );
